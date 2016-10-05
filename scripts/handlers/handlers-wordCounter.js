1582d655d53e0d81ad1c3c111eaf81ae2a303645
X00LA <X00LA@users.noreply.github.com>
2016-10-05 13:45:35 +0200

    Added missing files
    Added missing file.

---
+++ scripts/handlers/handlers-wordCounter.js
@@ -0,0 +1,7 @@
+$.lang.register('wordcounter.usage', 'Verwendung: !wordcounter [add / remove] !count [Wort]');
+$.lang.register('wordcounter.add.usage', 'Verwendung: !wordcounter add [Wort]');
+$.lang.register('wordcounter.added', ' wurde der Wort-Zähl-Liste hinzugefügt!');
+$.lang.register('wordcounter.remove.usage', 'Verwendung: !wordcounter remove [Wort]');
+$.lang.register('wordcounter.err.404', 'Dieses Wort ist nicht in der Zähler-Liste.');
+$.lang.register('wordcounter.removed', ' wurde aus der Zähler-Liste entfernt!');
+$.lang.register('wordcounter.count', '$1 wurde $2 Mal gesagt.');
