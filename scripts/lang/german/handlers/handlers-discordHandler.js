3532abdaeda43dcb73a8325d7d6350a26f852768
X00LA <xoolalp@gmail.com>
2016-11-11 21:31:58 +0100

    Changed Discord Message
    Changed Discord message to show also the channel avatar.

--- scripts/lang/german/handlers/handlers-discordHandler.js
+++ scripts/lang/german/handlers/handlers-discordHandler.js
@@ -1,1 +1,1 @@
-$.lang.register('discord.streamonline', '$1 hat gerade auf Twitch den Stream gestartet! Schau rein unter https://www.twitch.tv/$1!');
+$.lang.register('discord.streamonline', 'Hey @everyone! $1 hat gerade auf Twitch den Stream gestartet! Schau rein unter https://www.twitch.tv/$1 :wink:!');
